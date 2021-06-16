2a94f7656ab77f016b690e1e813c1c1865a110ab
Zehua.D <1047488275@qq.com>
2021-06-16 19:11:23 +0800

    base-0.02

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+
+//lottie的行为
 var animation = bodymovin.loadAnimation({
     container:document.getElementById('bm'),
     renderer:'svg',
@@ -7,8 +9,20 @@ var animation = bodymovin.loadAnimation({
 });
 
 //点击某个超链接，就在placeholder处显示该超链接的图片,whichpic是一个元素节点
-function showPic(whichpic){
+function showPic(whichpic,titlesReplace){
     var source = whichpic.getAttribute('href');
-    var placeholder = document.getElementById('placeholder');
-    placeholder.setAttribute('src',source);
+    var placeholderss = document.getElementById('placeholderss');
+    placeholderss.setAttribute('src',source);
+    
+    var titlesReplace = titlesReplace.getAttribute('title');
+    var description = document.getElementById('description');
+    description.setAttribute(titlesReplace);
 }
+
+//获取body的所有子节点
+// function countBodyChildren(){
+//     var body_element = document.getElementsByTagName('body')[0];
+//     alert(body_element.childNodes.length);
+// }
+
+// window.onload = countBodyChildren();
